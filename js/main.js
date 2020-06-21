9a21e1bb6d3647b031caa4e58b45ebd50836a972
a26232933 <>
2020-06-21 16:20:34 +0800

    完成

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-$('#banner').slick();
+AOS.init();
+
+$('#banner').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false
+});
+$('#brand').slick({
+    
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    arrows: false,
+    
+});
